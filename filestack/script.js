819c508b7caf793ecda688cd5d3f61f5b56ee8b5
btsalwa <brian.tsalwa@student.moringaschool.com>
2023-11-07 01:33:31 +0300

    having problem with upload to cloud,

--- filestack/script.js
+++ filestack/script.js
@@ -48,37 +48,5 @@ document.addEventListener('DOMContentLoaded', () => {
             imageDim: [500, 600]
         };
 
-        // Define security options (policy and signature)
-        const clientOptions = {
-            security: {
-                policy: "YOUR_POLICY_HERE", // Replace with your actual policy
-                signature: "YOUR_SIGNATURE_HERE" // Replace with your actual signature
-            }
-        };
-
-        // Create a new XMLHttpRequest to fetch data from a URL
-        const Http = new XMLHttpRequest();
-        const url = 'YOUR_DATA_URL_HERE'; // Replace with the actual URL
-
-        Http.open('GET', url);
-        Http.send();
-
-        // Open the Filestack picker with the specified options
-        const picker = client.picker(options, clientOptions);
-
-        // Handle file upload with the specified API URL
-        picker.open().then(function (response) {
-            if (response && response.filesUploaded && response.filesUploaded.length > 0) {
-                const fileUrl = response.filesUploaded[0].url;
-                const fileName = response.filesUploaded[0].filename;
-
-                // You can now send the file URL to your API endpoint (https://www.filestackapi.com/api/file)
-                // for further processing or storage.
-                console.log('File URL:', fileUrl);
-                console.log('File Name:', fileName);
-            } else {
-                console.error('No files uploaded.');
-            }
-        });
     });
 });
